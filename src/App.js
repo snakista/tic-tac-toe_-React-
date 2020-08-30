c40da973668819dbce6634cff3c15a669d4cee8d
snakista <sitanshyadav@gmail.com>
2020-08-30 16:10:45 +0530

    tic tac toe game

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+function App(){
+    return(
+        <div className="App">
+        <p>
+        Hello world
+        </p>
+        </div>
+    );
+}
+export default App;
